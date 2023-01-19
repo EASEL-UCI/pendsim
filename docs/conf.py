d8e65443ad8797c319e0cf27746ea3d1456ee755
EASEL Research Group at UCI <98562830+EASEL-UCI@users.noreply.github.com>
2023-01-19 12:15:30 -0800

    add multiprocess to mock imports

--- docs/conf.py
+++ docs/conf.py
@@ -68,7 +68,7 @@ templates_path = ["_templates"]
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
-autodoc_mock_imports = ["scipy", "numpy", "matplotlib", "cvxpy", "filterpy"]
+autodoc_mock_imports = ["scipy", "numpy", "matplotlib", "cvxpy", "filterpy", "multiprocess"]
 numpydoc_show_class_members = False
 
 # -- Options for HTML output -------------------------------------------------
